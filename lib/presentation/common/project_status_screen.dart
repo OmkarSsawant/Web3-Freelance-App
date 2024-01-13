8182d6f508344bf3f1d3254f9f9048708f6e39b0
OmkarSsawant <omkar18sawant@gmail.com>
2024-01-13 13:43:33 +0530

    Animated Widget Partially ready

--- lib/presentation/common/project_status_screen.dart
+++ lib/presentation/common/project_status_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:web3_freelancer/utils.dart';
 
 class ProjectStatusScreen extends StatefulWidget {
   const ProjectStatusScreen({super.key});
@@ -20,12 +22,21 @@ class _ProjectStatusScreenState extends State<ProjectStatusScreen> {
         title: const Text("Project Status"),
       ),
       body: SizedBox(
-        child: ListView.builder(
-            itemCount: 7,
-            itemBuilder: (context, index) {
-              return TrackingStepWidget(
-                  title: "title$index", date: "date$index", completed: true);
-            }),
+        width: context.screenWidth,
+        child: Padding(
+          padding: EdgeInsets.only(left: context.screenWidth * 0.05),
+          child: ListView.builder(
+              itemCount: 7,
+              itemBuilder: (context, index) {
+                return TrackingStepWidget(
+                    i: index,
+                    isLast: index == 6,
+                    title: "title$index",
+                    date: "0.$index eth",
+                    isNextCompleted: index < 3,
+                    completed: index < 4);
+              }),
+        ),
       ),
     );
   }
@@ -35,99 +46,90 @@ class TrackingStepWidget extends StatelessWidget {
   final String title;
   final String date;
   final bool completed;
-
-  const TrackingStepWidget({
-    required this.title,
-    required this.date,
-    required this.completed,
-  });
+  final int i;
+  final bool isLast;
+  final bool isNextCompleted;
+  const TrackingStepWidget(
+      {required this.title,
+      required this.date,
+      required this.completed,
+      required this.i,
+      required this.isLast,
+      required this.isNextCompleted});
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 400,
+      // height: 100,
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Column(
-            children: [
-              Container(
-                width: 24.0,
-                height: 24.0,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: completed ? Colors.green : Colors.grey,
-                ),
-              ),
-              LineAnimationWidget()
-            ],
+          TweenAnimationBuilder(
+              duration: Duration(milliseconds: 2000 * i),
+              tween: ColorTween(
+                  begin: Color.fromARGB(75, 150, 150, 150),
+                  end: !completed
+                      ? Color(0xff969696)
+                      : Color.fromARGB(255, 255, 143, 5)),
+              builder: (context, anim, child) {
+                return Column(
+                  children: [
+                    Container(
+                      width: 57.0,
+                      height: 57.0,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: anim,
+                      ),
+                    ),
+                    if (!isLast)
+                      Stack(
+                        children: [
+                          Container(
+                            width: 4,
+                            height: 37,
+                            color: Colors.grey,
+                          ),
+                          if (isNextCompleted)
+                            TweenAnimationBuilder<double>(
+                                duration: Duration(milliseconds: 2000 * i),
+                                tween: Tween(begin: 0, end: 47),
+                                builder: (context, animh, child) {
+                                  return Container(
+                                    width: 4,
+                                    height: animh,
+                                    color: anim,
+                                  );
+                                }),
+                        ],
+                      )
+                  ],
+                );
+              }),
+          SizedBox(width: 36.0),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 57),
+            child: Text(title,
+                style: TextStyle(fontSize: 27.0, fontWeight: FontWeight.w500)),
           ),
           SizedBox(width: 16.0),
-          Text(title, style: TextStyle(fontSize: 16.0)),
-          Spacer(),
-          Text(date, style: TextStyle(fontSize: 12.0, color: Colors.grey)),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 57.0),
+            child: Row(
+              children: [
+                SvgPicture.asset(
+                  "assets/images/eth.svg",
+                  width: 15,
+                ),
+                SizedBox(
+                  width: 7,
+                ),
+                Text(date, style: TextStyle(fontSize: 12.0, color: Colors.grey))
+              ],
+            ),
+          ),
         ],
       ),
     );
   }
-}
-
-class LineAnimationWidget extends StatefulWidget {
-  const LineAnimationWidget({Key? key}) : super(key: key);
-
-  @override
-  State<LineAnimationWidget> createState() => _LineAnimationWidgetState();
-}
-
-class _LineAnimationWidgetState extends State<LineAnimationWidget>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<Color?> _colorAnimation;
-  late Animation<double> _heightAnimation;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 3),
-    );
-    _colorAnimation = _controller.drive(ColorTween(
-      begin: Colors.grey,
-      end: Colors.green,
-    ));
-    _heightAnimation = _controller
-        .drive(CurveTween(curve: Curves.easeOut))
-        .drive(Tween<double>(begin: 0, end: 200));
-    _controller.forward();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: _colorAnimation,
-      builder: (BuildContext context, Widget? child) {
-        return CustomPaint(
-          painter: LinePainter(color: _colorAnimation.value),
-          size: Size(double.infinity, _heightAnimation.value),
-        );
-      },
-    );
-  }
-}
-
-class LinePainter extends CustomPainter {
-  final Color? color;
-
-  LinePainter({this.color});
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..color = color ?? Colors.grey
-      ..strokeWidth = 5;
-    canvas.drawLine(Offset.zero, Offset(0, size.height), paint);
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
