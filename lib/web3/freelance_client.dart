a1cf430d33823d2e3eedd84c1ea7f9e138a93bd6
OmkarSsawant <omkar18sawant@gmail.com>
2024-01-17 10:46:50 +0530

    Temp works on single Creds util Raised issue is resolved

--- lib/web3/freelance_client.dart
+++ lib/web3/freelance_client.dart
@@ -53,7 +53,7 @@ class FreelanceContractClient {
   Stream<FilterEvent>? _eventsStream;
   ContractAbi? abi;
   final Web3App _dapp;
-  Credentials get _mCreds => EthPrivateKey.fromHex("TODO");
+  Credentials get _mCreds => EthPrivateKey.fromHex("0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80");
 
   FreelanceContractClient(this._dapp) {
     _client = Client();
